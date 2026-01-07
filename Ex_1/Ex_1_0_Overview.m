834d1e0f6c54674311f2e73879cdf3f8f5b2ab1b
Diego.H.E <diegoh0201@gmail.com>
2026-01-07 10:02:41 +0100

    new files for ex1

---
+++ Ex_1/Ex_1_0_Overview.m
@@ -0,0 +1,18 @@
+%[text] # Class 1 - Simulink Introduction
+%[text] ## Learning Objectives
+%[text] 1. Be able to model dynamic systems using the functionalities integrated in Simulink
+%[text] 2. Understand the derivation of the "Equations of Motion" as preparation for the next class \
+%%
+%[text] ## Preparation
+%[text] - Complete the indicated modules of the Simulink Onboarding \
+%%
+%[text] ## Scripts with individual tasks
+%[text] - [Implementation of a simple Bouncing Ball Simulation](file:.\Ex_1_1_Bouncing_Ball.m) \
+%%
+%[text] 
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"inline","rightPanelPercent":40}
+%---
