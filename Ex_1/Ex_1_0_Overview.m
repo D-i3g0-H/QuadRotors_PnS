00299e33635c35ee642b29ca60fd605a61ac8c2b
Diego.H.E <diegoh0201@gmail.com>
2026-01-20 21:48:26 +0100

    added different exercise for students, more creative and can not find the solution directly online

--- Ex_1/Ex_1_0_Overview.m
+++ Ex_1/Ex_1_0_Overview.m
@@ -3,11 +3,19 @@
 %[text] 1. Be able to model dynamic systems using the functionalities integrated in Simulink
 %[text] 2. Understand the derivation of the "Equations of Motion" as preparation for the next class \
 %%
+%[text] ## System Check
+%[text] For the coding exercises you require the following toolboxes including their dependencies:
+%[text] - Simulink Control Design (depends on Control System Toolbox and Simulink)
+%[text] - Simscape Multibody (depends on Simulink and Simscape) \
+%[text] The following script will check your installation. If a package is missing it will open the Add-On Explorer from where you can install it.
+%[text] Please note: You might have to rerun this command if multiple packages are missing.
+checkInstallation %[output:52ff4b99]
+%%
 %[text] ## Preparation
-%[text] - Complete the indicated modules of the Simulink Onboarding \
+%[text] - Complete the indicated modules of the Simulink Onramp \
 %%
 %[text] ## Scripts with individual tasks
-%[text] - [Implementation of a simple Bouncing Ball Simulation](file:.\Ex_1_1_Bouncing_Ball.m) \
+%[text] - [Generating a PWM signal for Thrust](file:.\Ex_1_1_Thrust_Command.m) \
 %%
 %[text] 
 
@@ -15,4 +23,7 @@
 %---
 %[metadata:view]
 %   data: {"layout":"inline","rightPanelPercent":40}
+%---
+%[output:52ff4b99]
+%   data: {"dataType":"text","outputData":{"text":"MATLAB version OK\n \nSimulink Control Design and its dependencies are installed\nSimscape Multibody and its dependencies are installed\n \n","truncated":false}}
 %---
---
+++ Ex_1/Ex_1_1_Thrust_Command.m
@@ -0,0 +1,8 @@
+%[text] ##  1.1 Simulation of Thrust Command
+%[text] 
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"inline","rightPanelPercent":40}
+%---
